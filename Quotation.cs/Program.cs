6cfd48101bf2b7144d07cfc055c5c2d6b88e88cd
shookt <tshook428@gmail.com>
2015-09-17 11:19:25 -0400

    new stuff

---
+++ Quotation.cs/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quotation.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You look like a GODDAMN half-baked potato.");
+            Console.WriteLine("                                            -Tyler Shook, 2k15");
+        }
+    }
+}
