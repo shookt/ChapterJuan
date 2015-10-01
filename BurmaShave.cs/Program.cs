19e349b71f37c171856c4006a685d938059466fc
shookt <tshook428@gmail.com>
2015-10-01 11:11:42 -0400

    final commit

---
+++ BurmaShave.cs/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BurmaShave.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Little Willie");
+            Console.WriteLine("Modern soul");
+            Console.WriteLine("Busted Papa's");
+            Console.WriteLine("Brush and Bowl");
+            Console.WriteLine("Nice Work Willie");
+            Console.WriteLine("Burma-Shave");
+        }
+    }
+}
